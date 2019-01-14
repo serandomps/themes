180edc8a4b097b4a70ddfb8b07d7023d43bfec2e
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-01-14 08:43:19 +0530

    updating

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var dust = require('dust');
+var dust = require('dust')();
 
-var blocks = [
-    'checkbox',
-    'email',
-    'password',
-    'radio',
-    'select',
-    'text',
-    'textarea'
-];
-
-blocks.forEach(function (name) {
-    dust.loadSource(dust.compile(require('./' + name), 'blocks-' + name));
-});
+dust.loadSource(dust.compile(require('./blocks/checkbox'), 'themes-blocks-checkbox'));
+dust.loadSource(dust.compile(require('./blocks/checkboxes'), 'themes-blocks-checkboxes'));
+dust.loadSource(dust.compile(require('./blocks/email'), 'themes-blocks-email'));
+dust.loadSource(dust.compile(require('./blocks/password'), 'themes-blocks-password'));
+dust.loadSource(dust.compile(require('./blocks/radios'), 'themes-blocks-radios'));
+dust.loadSource(dust.compile(require('./blocks/select'), 'themes-blocks-select'));
+dust.loadSource(dust.compile(require('./blocks/select-options'), 'themes-blocks-select-options'));
+dust.loadSource(dust.compile(require('./blocks/text'), 'themes-blocks-text'));
+dust.loadSource(dust.compile(require('./blocks/textarea'), 'themes-blocks-textarea'));
+dust.loadSource(dust.compile(require('./blocks/uploads'), 'themes-blocks-uploads'));
+dust.loadSource(dust.compile(require('./blocks/uploads-preview'), 'themes-blocks-uploads-preview'));
